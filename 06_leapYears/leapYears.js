3e9c7c71b83f11d062099d0153b936dc7cb6b21a
Andre Carr <129695312+Andre-Carr@users.noreply.github.com>
2023-10-30 19:02:39 -0400

    Add leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear = false;
+    if (!(year % 4) && (year % 100)) {
+        isLeapYear = true;
+    } else if (!(year % 400)) {
+        isLeapYear = true;
+    }
+    return isLeapYear;   
 };
 
 // Do not edit below this line
