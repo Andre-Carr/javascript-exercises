29f3e7328c97c6c32dc9d9522ceb17122d259719
Andre Carr <129695312+Andre-Carr@users.noreply.github.com>
2023-10-30 17:42:35 -0400

    Add repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let strOutput = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++) {
+        strOutput += str;
+    }
+    return strOutput;
 };
 
 // Do not edit below this line
