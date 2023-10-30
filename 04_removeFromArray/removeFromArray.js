c2e38b7f25bfe7bac558b4d2a04e8e3234cc7d70
Andre Carr <129695312+Andre-Carr@users.noreply.github.com>
2023-10-30 18:14:10 -0400

    Add removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    args.forEach( (item) => {
+        if( array.includes(item)) {
+            array.splice(array.indexOf(item), 1);
+        }
+    });
 
+    return array;
 };
 
 // Do not edit below this line
