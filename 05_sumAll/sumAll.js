3fa064c0e211372acbf32b237466e7f36befc80e
Andre Carr <129695312+Andre-Carr@users.noreply.github.com>
2023-10-30 18:40:05 -0400

    Add sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(intOne, intTwo) {
+    if(typeof(intOne) !== 'number' || typeof(intTwo) !== 'number') return "ERROR";
+    if(intOne < 0 || intTwo < 0) return "ERROR";
+    const min = (intOne < intTwo) ? intOne : intTwo;
+    const max = (intOne > intTwo) ? intOne : intTwo;
+    let sum = 0;
+    for(let addend = min; addend <= max; addend++) {
+        sum += addend;
+    }
+    return sum;
 };
 
 // Do not edit below this line
