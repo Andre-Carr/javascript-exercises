e70734707fb0cce80bdf290173dd75b2889c9f2a
Andre Carr <129695312+Andre-Carr@users.noreply.github.com>
2023-11-02 05:21:45 -0400

    Change logic from using sort to using reduce. Doesn't make sense to sort the whole array if only one value of the array is relevant.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,13 +1,5 @@
 const findTheOldest = function(people) {
-    /*
-    const oldToYoung = people.sort(
-        (a, b) => {
-            const aAge = (a.yearOfDeath) ? a.yearOfDeath - a.yearOfBirth : new Date().getFullYear() - a.yearOfBirth;
-            const bAge = (b.yearOfDeath) ? b.yearOfDeath - b.yearOfBirth : new Date().getFullYear() - b.yearOfBirth;
-            return aAge > bAge ? -1 : 1;
-        });
-    return oldToYoung[0];
-    */
+    
     const oldest = people.reduce(
         (oldest, person) => {
             const oldestAge = (oldest.yearOfDeath) ? oldest.yearOfDeath - oldest.yearOfBirth : new Date().getFullYear() - oldest.yearOfBirth;
