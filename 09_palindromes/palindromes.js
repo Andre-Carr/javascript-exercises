5d63f4d59d6b1d2004d8b11a860745d0266aee11
Andre Carr <129695312+Andre-Carr@users.noreply.github.com>
2023-11-02 03:29:21 -0400

    Add palindrome checking functionality

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    // convert the string into an Array to make it easier to reverse the second half
+    const noPunct = Array.from(phrase.toLowerCase()).filter(
+        (letter) => (letter.charCodeAt(0) >= 48 && letter.charCodeAt(0) <= 57) 
+        || (letter.charCodeAt(0) >=97 && letter.charCodeAt(0) <= 122)
+    );
 
+    let firstHalf = '';
+    let lastHalf = '';
+    if (noPunct.length % 2) {
+         firstHalf = noPunct.slice(0, noPunct.length / 2).join('');
+         lastHalf = noPunct.slice(noPunct.length / 2 + 1).reverse().join('');
+    } else {
+         firstHalf = noPunct.slice(0, noPunct.length / 2).join('');
+         lastHalf = noPunct.slice(noPunct.length / 2).reverse().join('');
+    }
+    console.log(`****\n${noPunct.length}\n${firstHalf} \n${lastHalf}\n****\n`)
+    return firstHalf === lastHalf;
 };
 
 // Do not edit below this line
