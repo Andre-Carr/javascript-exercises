2c81ea97efb4beabe7ecece858dc5e1192884402
Andre Carr <129695312+Andre-Carr@users.noreply.github.com>
2023-11-02 03:57:47 -0400

    Implement fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequence) {
+    const seqNum = parseInt(sequence);
+    if(seqNum < 0 ) return "OOPS";
+    if(seqNum === 0) return 0;
+    if(seqNum === 1 || seqNum === 2) return 1;
+    return fibonacci(seqNum- 1) + fibonacci(seqNum - 2);
 };
 
 // Do not edit below this line
